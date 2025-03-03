2b7917220294996db5501399ab2b08af74f1a023
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2025-03-03 16:06:40 +0000

    updated fetch demo to include form.

--- week-05/02-fetch/app.js
+++ week-05/02-fetch/app.js
@@ -54,3 +54,42 @@ refreshBtn.addEventListener("click", function () {
 });
 
 getFoxesOntoPage();
+
+// Tying forms and fetch together
+// A user can input a number on a page DONE
+// a minimum of 0, and a maximum of 923042 DONE
+// they click a button, and a fetch request is made with that number, to the museum endpoint DONE
+// where [objectID] is replaced with our number. DONE!
+// https://collectionapi.metmuseum.org/public/collection/v1/objects/[objectID]
+
+// I need to get the users number:
+const userForm = document.getElementById("user-input-form");
+userForm.addEventListener("submit", getUserInput);
+
+// This function is fired on the event listener. It automatically takes the 'event', and we get the users input from it.
+// Once it has done that, it also invokes the fetchMuseumImage function and *passes* the users input to it
+function getUserInput(event) {
+  event.preventDefault();
+  console.log(event.target.museumNumber.value);
+
+  const userNumber = event.target.museumNumber.value;
+
+  fetchMuseumImage(userNumber);
+}
+
+const museumImage = document.getElementById("museum-image");
+
+// This function takes the users input, makes a dynamic fetch request with it
+// using our backtick and template literal friends.
+// then it changes the image on the page.
+// the only thing it doesn't do right now is handle if an image doesn't exist.
+async function fetchMuseumImage(userNumber) {
+  console.log("I am logging the userNumber in fetchMuseumImage", userNumber);
+  const response = await fetch(
+    `https://collectionapi.metmuseum.org/public/collection/v1/objects/${userNumber}`
+  );
+  const data = await response.json();
+  // console.log("museum data is", data.primaryImage);
+  museumImage.src = data.primaryImage;
+  museumImage.width = 300;
+}
