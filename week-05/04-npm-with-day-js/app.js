c9d2481a847e531a4644ee2a41a0e976cfec1f9e
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2025-03-04 11:18:13 +0000

    npm with dayjs demo

---
+++ week-05/04-npm-with-day-js/app.js
@@ -0,0 +1,10 @@
+// const dayjs = require("dayjs"); // this is the old syntax. Boo. Ugly. Rubbish. Go in the bin
+
+// We want the shiny new ES6 syntax:
+import dayjs from "dayjs";
+
+console.log("Get the Hour:", dayjs().hour());
+
+console.log(dayjs().day());
+
+// Remember to create the .gitignore file, and add into it any files/folders you don't want to push to github!
