9e352d2e64984c65129fdb7b3d4bf3b0c3ef9302
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2025-03-04 10:33:20 +0000

    default exports, plus variable reassignment

--- week-05/03-import-export/app.js
+++ week-05/03-import-export/app.js
@@ -5,14 +5,18 @@
 // and that location must be: in "double quotes", hav a file pathway, and end in the file extension = .js
 import { multiply } from "./functions.js";
 import { x, y, myName } from "./variables.js";
-import { increaseCount, count } from "./counter.js";
+import { setCount, count, countState, setAnyNumber } from "./counter.js"; // This is called a named import/export.
+import someThing from "./defaultExportThing.js"; // This is called a default export.
 
 // get the elements from our HTML:
 const myNameH1 = document.getElementById("my-name");
-const myBookDiv = document.getElementById("my-books");
+const myLocationH3 = document.getElementById("my-location");
 
 const myCount = document.getElementById("my-counter");
 const myButton = document.getElementById("my-button");
+
+const mySecondCount = document.getElementById("my-second-counter");
+const mySecondButton = document.getElementById("my-second-button");
 
 // What we're used to:
 const num1 = 10;
@@ -41,8 +45,26 @@ myCount.textContent = count;
 myButton.addEventListener("click", function () {
   console.log("the button is clicked:", count);
   // Everytime the button is clicked, increase the counter by 1
-  increaseCount();
+  setCount();
   console.log("The function has finished running", count);
   // and update the value on the page
   myCount.textContent = count;
+});
+
+// This is my 'default-ly' exported function.
+myLocationH3.textContent = someThing();
+
+// Let's look at the second counter
+mySecondCount.textContent = countState.count;
+
+// Think back to objects. We can add values to an abject, and even change them. But we can't change a whole object to a number, for example.:
+countState.myName = "Frankie";
+countState.myPet = "Luna";
+countState.colours = ["blue", "red", 25];
+
+mySecondButton.addEventListener("click", function () {
+  console.log("countState is an object with a property:", countState);
+  // We must remember to reassign the value to the result of the function:
+  countState.count = setAnyNumber(countState.count, 10);
+  mySecondCount.textContent = countState.count;
 });
