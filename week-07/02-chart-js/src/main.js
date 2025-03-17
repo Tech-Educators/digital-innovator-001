6a1025116e91864022a399e26951fd4d671837b6
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2025-03-17 13:44:17 +0000

    chatjs with map method

--- week-07/02-chart-js/src/main.js
+++ week-07/02-chart-js/src/main.js
@@ -40,3 +40,30 @@ new Chart(canvas, {
     },
   },
 });
+
+const chartData = [
+  { year: 2010, count: 10, count2: 1 },
+  { year: 2011, count: 20, count2: 12 },
+  { year: 2012, count: 15, count2: 8 },
+  { year: 2013, count: 25, count2: 17 },
+  { year: 2014, count: 22, count2: 7 },
+  { year: 2015, count: 10, count2: 19 },
+  { year: 2016, count: 28, count2: 9 },
+];
+
+new Chart(document.getElementById("acquisitions"), {
+  type: "bar",
+  data: {
+    labels: chartData.map((row) => row.year), // Output of this .map: "[2010, 2011, 2012, 2013, 2014, etc etc ]""
+    datasets: [
+      {
+        label: "Acquisitions by year",
+        data: chartData.map((row) => row.count),
+      },
+      {
+        label: "Sales by year",
+        data: chartData.map((row) => row.count2),
+      },
+    ],
+  },
+});
